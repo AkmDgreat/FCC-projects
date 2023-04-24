41faac916ba9b1ec2816630811ae13cedb25e43c
AkmDgreat <admiralakshat@gmail.com>
2023-04-24 15:30:11 +0530

    update

--- Backend and API project/01-Timestamp microservice/index.js
+++ Backend and API project/01-Timestamp microservice/index.js
@@ -1,36 +1,26 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
 const router = require("./Router/route")
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-//var cors = require('cors');
-//app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors")
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-//app.use(express.static('public'));
+app.use(express.static("public"))
 
 app.use(express.json())
 app.use(router)
 
-// http://expressjs.com/en/starter/basic-routing.html
-/* app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-}); */
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  res.json({ greeting: "hello API" })
+})
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 5000, function () { //CHANGES MADE
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+var listener = app.listen(process.env.PORT || 5000, function () {
+  //CHANGES MADE
+  console.log("Your app is listening on port " + listener.address().port)
+})
